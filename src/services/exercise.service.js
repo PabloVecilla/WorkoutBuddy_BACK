af805d8f1eddc703139328f0ae5c646cfd62b8f5
PabloVecilla <pablovecilla8@gmail.com>
2026-09-02 17:26:20 +0200

    PV_pagination for exercise findAll

--- src/services/exercise.service.js
+++ src/services/exercise.service.js
@@ -1,11 +1,12 @@
 const { Exercise } = require("../models");  
 
-const findAllExercises = async (filters = {}) => {
-    return await Exercise.findAll({ where: filters });
+const findAllExercises = async (page, limit) => {
+    const total = await Exercise.count(); 
+    if (!page || page === 0 || page*10 > total) page = 1; 
+    return await Exercise.findAndCountAll({ limit, offset: (page-1)*limit });
 }; 
 
 const findExerciseById = async (id) => {
-
     return await Exercise.findByPk(id);
 }; 
 
