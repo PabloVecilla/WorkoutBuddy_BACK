0ca6c7b69873104dab329720cc69fb978377fa3c
PabloVecilla <pablovecilla8@gmail.com>
2026-09-02 18:05:25 +0200

    PV_add get/programs test

--- tests/program.test.js
+++ tests/program.test.js
@@ -3,18 +3,19 @@ const app = require("../src/app");
 const { Program } = require("../src/models"); 
 
 describe ("Program", () => {
+    const programCreationData = {
+        name: "Summer ready", 
+        goal: "recomp", 
+        level: "intermediate",
+        frequency: 5
+    }; 
+    const validUserData = {
+        name: "Carla",
+        email: "carla@example.com",
+        password: "123456"
+    }; 
     describe("POST/ programs/create", () => {
-        const programCreationData = {
-            name: "Summer ready", 
-            goal: "recomp", 
-            level: "intermediate",
-            frequency: 5
-        }; 
-        const validUserData = {
-            name: "Carla",
-            email: "carla@example.com",
-            password: "123456"
-        }; 
+
         it("authenticated user creates a program", async () => {
             const agent = request.agent(app); // keeps active session cookies from register and login
 
@@ -34,5 +35,19 @@ describe ("Program", () => {
 
         }); 
     }); 
-    // describe("GET /programs")
+    describe("GET /programs", () => {
+        it("returns programs for user", async () => {
+            const agent = request.agent(app); // keeps active session cookies from register and login
+
+            await agent.post("/auth/register").send(validUserData); 
+            const user = await agent.post("/auth/login").send({ email: validUserData.email, password: validUserData.password }); 
+
+            await agent.post("/programs/create").send(programCreationData); 
+
+            const response = await agent.get("/programs"); 
+
+            expect(response.status).toBe(200); 
+            expect(response.body[0].userId).toBe(user.body.user.id); 
+        })
+    })
 })
