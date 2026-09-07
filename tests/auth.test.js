6c7897075643a0919a7368701755dc558a0898c0
PabloVecilla <pablovecilla8@gmail.com>
2026-09-07 20:09:20 +0200

    PV_FE readiness

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -131,6 +131,8 @@ describe ("Authentication", () => {
             await request(app).post("/auth/login").send({ email: invalidUserData.email, password: invalidUserData.password }); 
             const response = await request(app).post("/auth/login").send({ email: invalidUserData.email, password: invalidUserData.password }); 
 
+            console.log("RESPONSE: ", response); 
+
             expect(response.status).toBe(429); 
             expect(response.body).toEqual({
                 success: false,
